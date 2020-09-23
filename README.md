bf5d25255ae6f8920d587c7aedf939e10652e30e
kevinchen2046 <kevin-chen@foxmail.com>
2020-09-23 18:25:15 +0800

    'x'

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # game-service
 游戏自动化构建服务
+
+## 配置(package.json)
+```json
+{
+    "workpath": {
+        /**客户端路径*/
+        "client": "E:/project/xxj_dev/client",
+        /**服务端路径*/
+        "server": "E:/project/xxj_dev/server",
+        /**策划表路径*/
+        "excel": "E:/project/xxj_dev/xls",
+        /**客户端配置输出路径*/
+        "client-config": "E:/project/xxj_dev/client/resource/config",
+        /**服务端配置输出路径*/
+        "server-config": "E:/project/xxj_dev/server_build/conftab",
+        /**服务端配置发布输出路径*/
+        "server-release-config": "C:/Users/Administrator/AppData/MixE/xxj_dev/game_version/conftab",
+        /**协议路径*/
+        "proto":"E:/project/xxj_dev/proto",
+        /**客户端协议输出路径*/
+        "client-proto":"E:/project/xxj_dev/client/libs/proto",
+        /**服务端协议输出路径*/
+        "server-proto":"E:/project/xxj_dev/server/src/lib/proto"
+    },
+    "template": {
+        /**协议默认模板文件路径*/
+        "proto": "template/proto"
+    },
+    "appport": {
+        /**客户端访问服务端口*/
+        "client-service": 7888,
+        /**控制台前端服务端口*/
+        "console-service": 9888,
+        /**控制台后端服务端口*/
+        "build-service": 14739
+    }
+}
+```
+
+## 技术说明
+- 客户端访问服务
+  - http-server 
+- 控制台访问页面
+  - vue.js
+  - element-ui
+- 控制台后端服务
+  - websocket
+## 启动 
+```
+node services-socket
+```
--- src/proto.js
+++ src/proto.js
@@ -7,124 +7,128 @@ var logger = require('./logger');
 var template_dts = fs.readFileSync(`${__dirname}/../${config.template.proto}/proto.d.ts`, 'utf-8');
 var template_js = fs.readFileSync(`${__dirname}/../${config.template.proto}/proto.js`, 'utf-8');
 
-function parse(content) {
-    var clazznames = content.match(/(?<=class)([\S\s]*?)(?={)/g);
-    var clazzcontents = content.match(/(?<={)([\S\s]*?)(?=})/g);
-    if (clazznames.length != clazzcontents.length) {
-        console.error('解析错误...');
-        return;
-    }
-    var results = {};
-    for (var i = 0; i < clazznames.length; i++) {
-        var properties = results[clazznames[i].replace(/ /g, '')] = [];
-        var lines = clazzcontents[i].split('\r\n');
-        var notes;
-        for (var line of lines) {
-            var linecontent = line.replace(/ /g, '');
-            if (!linecontent) continue;
-            var isnote = !!linecontent.match(/^[\/{1}*{2},\/{1}\/{2},*{1}]/);
-            if (isnote) {
-                if (!notes) notes = [];
-                notes.push(linecontent);
-            } else {
-                var p = linecontent.split(':');
-                if (p.length != 2) {
-                    console.error('解析错误:', clazznames[i]);
-                    continue;
+class TypeScriptParser{
+    static parse(content) {
+        var clazznames = content.match(/(?<=class)([\S\s]*?)(?={)/g);
+        var clazzcontents = content.match(/(?<={)([\S\s]*?)(?=})/g);
+        if (clazznames.length != clazzcontents.length) {
+            console.error('解析错误...');
+            return;
+        }
+        var results = {};
+        for (var i = 0; i < clazznames.length; i++) {
+            var properties = results[clazznames[i].replace(/ /g, '')] = [];
+            var lines = clazzcontents[i].split('\r\n');
+            var notes;
+            for (var line of lines) {
+                var linecontent = line.replace(/ /g, '');
+                if (!linecontent) continue;
+                var isnote = !!linecontent.match(/^[\/{1}*{2},\/{1}\/{2},*{1}]/);
+                if (isnote) {
+                    if (!notes) notes = [];
+                    notes.push(linecontent);
+                } else {
+                    var p = linecontent.split(':');
+                    if (p.length != 2) {
+                        console.error('解析错误:', clazznames[i]);
+                        continue;
+                    }
+                    properties.push({
+                        filed: p[0].replace(/ /g, ''),
+                        type: p[1].replace(/ /g, ''),
+                        note: notes ? notes.join('').replace(/\//g, '').replace(/\*/g, '') : ''
+                    })
+                    notes = null;
                 }
-                properties.push({
-                    filed: p[0].replace(/ /g, ''),
-                    type: p[1].replace(/ /g, ''),
-                    note: notes ? notes.join('').replace(/\//g, '').replace(/\*/g, '') : ''
-                })
-                notes = null;
             }
         }
+        return results;
     }
-    return results;
 }
 
-function createdts(files) {
-    var start = '///////////////////////GENERATE-START/////////////////////';
-    var end = '///////////////////////GENERATE-END/////////////////////';
-    var sindex = template_dts.indexOf(start) + start.length;
-    var eindex = template_dts.indexOf(end) - 1;
-    var replacetext = template_dts.substring(sindex, eindex);
-    var moduleformat = `
-declare namespace proto.{modulename} {
-    {content}
-}`
-    var classformat = `
-    class {classname} extends proto.Message {
+class Generater{
+    static createdts(files) {
+        var start = '///////////////////////GENERATE-START/////////////////////';
+        var end = '///////////////////////GENERATE-END/////////////////////';
+        var sindex = template_dts.indexOf(start) + start.length;
+        var eindex = template_dts.indexOf(end) - 1;
+        var replacetext = template_dts.substring(sindex, eindex);
+        var moduleformat = `
+    declare namespace proto.{modulename} {
         {content}
     }`
-    var contents = [];
-    for (var name in files) {
-        var file = files[name];
-        var filecontents = [];
-        for (var classname in file) {
-            if (classname == '__moduleid__') continue;
-            var fileds = file[classname];
-            var result = [];
-            for (var filed of fileds) {
-                if (filed.filed == '__cmd__') continue;
-                if (filed.note) {
-                    result.push(`/**${filed.note}**/`)
+        var classformat = `
+        class {classname} extends proto.Message {
+            {content}
+        }`
+        var contents = [];
+        for (var name in files) {
+            var file = files[name];
+            var filecontents = [];
+            for (var classname in file) {
+                if (classname == '__moduleid__') continue;
+                var fileds = file[classname];
+                var result = [];
+                for (var filed of fileds) {
+                    if (filed.filed == '__cmd__') continue;
+                    if (filed.note) {
+                        result.push(`/**${filed.note}**/`)
+                    }
+                    result.push(`${filed.filed}:${filed.type};`)
                 }
-                result.push(`${filed.filed}:${filed.type};`)
+                filecontents.push(classformat.replace('{classname}', classname).replace('{content}', result.join('\r\n        ')));
             }
-            filecontents.push(classformat.replace('{classname}', classname).replace('{content}', result.join('\r\n        ')));
+            contents.push(moduleformat.replace('{modulename}', name).replace('{content}', filecontents.join('\r\n    ')))
         }
-        contents.push(moduleformat.replace('{modulename}', name).replace('{content}', filecontents.join('\r\n    ')))
+        return template_dts.replace(replacetext, contents.join('\r\n'));
     }
-    return template_dts.replace(replacetext, contents.join('\r\n'));
-}
-
-function createjs(files) {
-    var start = '///////////////////////GENERATE-START/////////////////////';
-    var end = '///////////////////////GENERATE-END/////////////////////';
-    var sindex = template_js.indexOf(start) + start.length;
-    var eindex = template_js.indexOf(end);
-    var replacedefinetext = template_js.substring(sindex, eindex);
-
-    start = '///////////////////////Map-START[Server]/////////////////////';
-    end = '///////////////////////Map-End[Server]/////////////////////'
-    sindex = template_js.indexOf(start) + start.length;
-    eindex = template_js.indexOf(end);
-    var replacemaptext = template_js.substring(sindex, eindex);
-
-    var moduleformat = `
-    var {modulename};
-    (function (bag) {
-        {modulename}.__moduleid__ = {moduleid};
-    {content}
-    })({modulename} || ({modulename} = {}));
-    proto.{modulename} = {modulename};`;
-    var classformat = `    {modulename}.{classname} = {msgid};`;
-    var mapformat = `    proto.__map__[{modulename}.{classname}] = bag.__moduleid__;`
-    var contents = [];
-    var mapcontents = [];
-    for (var name in files) {
-        var file = files[name];
-        var filecontents = [];
-        for (var classname in file) {
-            if (classname == '__moduleid__') continue;
-            var fileds = file[classname];
-            console.log(fileds)
-            var msgid;
-            for (var filed of fileds) {
-                if (filed.filed == '__cmd__') {
-                    msgid = filed.type;
-                    continue;
+    
+    static createjs(files) {
+        var start = '///////////////////////GENERATE-START/////////////////////';
+        var end = '///////////////////////GENERATE-END/////////////////////';
+        var sindex = template_js.indexOf(start) + start.length;
+        var eindex = template_js.indexOf(end);
+        var replacedefinetext = template_js.substring(sindex, eindex);
+    
+        start = '///////////////////////Map-START[Server]/////////////////////';
+        end = '///////////////////////Map-End[Server]/////////////////////'
+        sindex = template_js.indexOf(start) + start.length;
+        eindex = template_js.indexOf(end);
+        var replacemaptext = template_js.substring(sindex, eindex);
+    
+        var moduleformat = `
+        var {modulename};
+        (function (bag) {
+            {modulename}.__moduleid__ = {moduleid};
+        {content}
+        })({modulename} || ({modulename} = {}));
+        proto.{modulename} = {modulename};`;
+        var classformat = `    {modulename}.{classname} = {msgid};`;
+        var mapformat = `    proto.__map__[{modulename}.{classname}] = bag.__moduleid__;`
+        var contents = [];
+        var mapcontents = [];
+        for (var name in files) {
+            var file = files[name];
+            var filecontents = [];
+            for (var classname in file) {
+                if (classname == '__moduleid__') continue;
+                var fileds = file[classname];
+                console.log(fileds)
+                var msgid;
+                for (var filed of fileds) {
+                    if (filed.filed == '__cmd__') {
+                        msgid = filed.type;
+                        continue;
+                    }
                 }
+                filecontents.push(classformat.replace('{modulename}', name).replace('{classname}', classname).replace('{msgid}', msgid));
+                mapcontents.push(mapformat.replace('{modulename}', name).replace('{classname}', classname));
             }
-            filecontents.push(classformat.replace('{modulename}', name).replace('{classname}', classname).replace('{msgid}', msgid));
-            mapcontents.push(mapformat.replace('{modulename}', name).replace('{classname}', classname));
+            contents.push(moduleformat.replace(/{modulename}/g, name).replace('{moduleid}', file['__moduleid__']).replace('{content}', filecontents.join('\r\n    ')))
         }
-        contents.push(moduleformat.replace(/{modulename}/g, name).replace('{moduleid}', file['__moduleid__']).replace('{content}', filecontents.join('\r\n    ')))
+        console.log(mapcontents)
+        return template_js.replace(replacedefinetext, contents.join('\r\n') + '\r\n    ').replace(replacemaptext, '\r\n' + mapcontents.join('\r\n') + '\r\n    ');
     }
-    console.log(mapcontents)
-    return template_js.replace(replacedefinetext, contents.join('\r\n') + '\r\n    ').replace(replacemaptext, '\r\n' + mapcontents.join('\r\n') + '\r\n    ');
 }
 
 module.exports = async function () {
@@ -145,13 +149,13 @@ module.exports = async function () {
                 name = a[0];
                 moduleid = a[1];
             }
-            results[name] = parse(content);
+            results[name] = TypeScriptParser.parse(content);
             results[name]['__moduleid__'] = moduleid;
         }
         logger.log('生成定义文件...')
-        var filets = createdts(results);
+        var filets = Generater.createdts(results);
         logger.log('生成映射文件...')
-        var filejs = createjs(results)
+        var filejs = Generater.createjs(results)
 
         logger.log('写入客户端协议...')
         utils.createFolder(`${config["workpath"]["client-proto"]}`);
